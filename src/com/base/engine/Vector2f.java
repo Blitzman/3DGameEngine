e275decffc9cb119755f1f66d4690fff09940fb9
Albert García <albertgg93@gmail.com>
2013-06-17 22:05:41 +0200

    Vector Classes
    New classes for representing vectors have been added. Two dimensional
    representation has been completed with many new operations and three
    dimensional one is completely new with an extra method: the cross
    product.

--- src/com/base/engine/Vector2f.java
+++ src/com/base/engine/Vector2f.java
@@ -11,11 +11,65 @@ public class Vector2f
 		this.y = y;
 	}
 	
+	public float Length()
+	{
+		return (float)Math.sqrt(x*x + y*y);
+	}
+	public Vector2f Normalize()
+	{
+		return new Vector2f(x/Length(), y/Length());
+	}
+	public Vector2f Rotate(float angle)
+	{
+		float rad = (float)Math.toRadians(angle);
+		float cos = (float)Math.cos(rad);
+		float sin = (float)Math.sin(rad);
+		
+		return new Vector2f(x*cos - y*sin, x*sin + y*cos);
+	}
+	
+	public float Dot(Vector2f v)
+	{
+		return (x * v.x + y * v.y);
+	}
+	
+	public Vector2f Add(Vector2f v)
+	{
+		return new Vector2f(x + v.x, y + v.y);
+	}
+	public Vector2f Add(float a)
+	{
+		return new Vector2f(x + a, y + a);
+	}
+	public Vector2f Sub(Vector2f v)
+	{
+		return new Vector2f(x - v.x, y - v.y);
+	}
+	public Vector2f Sub(float a)
+	{
+		return new Vector2f(x - a, y - a);
+	}
+	public Vector2f Mul(Vector2f v)
+	{
+		return new Vector2f(x * v.x, y * v.y);
+	}
+	public Vector2f Mul(float a)
+	{
+		return new Vector2f(x * a, y * a);
+	}
+	public Vector2f Div(Vector2f v)
+	{
+		return new Vector2f(x / v.x, y / v.y);
+	}
+	public Vector2f Div(float a)
+	{
+		return new Vector2f(x / a, y / a);
+	}
+	
 	public float GetX()
 	{
 		return x;
 	}
-
 	public float GetY()
 	{
 		return y;
@@ -25,7 +79,6 @@ public class Vector2f
 	{
 		this.x = x;
 	}
-
 	public void SetY(float y)
 	{
 		this.y = y;
@@ -33,6 +86,6 @@ public class Vector2f
 	
 	public String toString()
 	{
-		return "(" + x + "," + y + ")";
+		return "(" + x + ", " + y + ")";
 	}
 }
---
+++ src/com/base/engine/Vector3f.java
@@ -0,0 +1,109 @@
+package com.base.engine;
+
+public class Vector3f
+{
+	private float x;
+	private float y;
+	private float z;
+	
+	public Vector3f(float x, float y, float z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public float Length()
+	{
+		return (float)Math.sqrt(x*x + y*y + z*z);
+	}
+	public Vector3f Normalize()
+	{
+		return new Vector3f(x/Length(), y/Length(), z/Length());
+	}
+	public Vector2f Rotate(float angle)
+	{
+		float rad = (float)Math.toRadians(angle);
+		float cos = (float)Math.cos(rad);
+		float sin = (float)Math.sin(rad);
+		
+		return new Vector2f(x*cos - y*sin, x*sin + y*cos);
+	}
+	
+	public float Dot(Vector3f v)
+	{
+		return (x * v.x + y * v.y + z * v.z);
+	}
+	public Vector3f Cross(Vector3f v)
+	{
+		float resX = y * v.z - z * v.y;
+		float resY = z * v.x - x * v.z;
+		float resZ = x * v.y - y * v.x;
+		
+		return new Vector3f(resX, resY, resZ);
+	}
+	
+	public Vector3f Add(Vector3f v)
+	{
+		return new Vector3f(x + v.x, y + v.y, z - v.z);
+	}
+	public Vector3f Add(float a)
+	{
+		return new Vector3f(x + a, y + a, z + a);
+	}
+	public Vector3f Sub(Vector3f v)
+	{
+		return new Vector3f(x - v.x, y - v.y, z - v.z);
+	}
+	public Vector3f Sub(float a)
+	{
+		return new Vector3f(x - a, y - a, z - a);
+	}
+	public Vector3f Mul(Vector3f v)
+	{
+		return new Vector3f(x * v.x, y * v.y, z * v.z);
+	}
+	public Vector3f Mul(float a)
+	{
+		return new Vector3f(x * a, y * a, z * a);
+	}
+	public Vector3f Div(Vector3f v)
+	{
+		return new Vector3f(x / v.x, y / v.y, z / v.z);
+	}
+	public Vector3f Div(float a)
+	{
+		return new Vector3f(x / a, y / a, z / a);
+	}
+	
+	public float GetX()
+	{
+		return x;
+	}
+	public float GetY()
+	{
+		return y;
+	}
+	public float GetZ()
+	{
+		return z;
+	}
+
+	public void SetX(float x)
+	{
+		this.x = x;
+	}
+	public void SetY(float y)
+	{
+		this.y = y;
+	}
+	public void SetZ(float z)
+	{
+		this.z = z;
+	}
+	
+	public String toString()
+	{
+		return "(" + x + ", " + y + ", " + z + ")";
+	}
+}
